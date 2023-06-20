ab518fc10c96ccf16dcb6b928b10c53af2ef8afe
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-20 15:40:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,17 @@
  */
 char print_alphabet(void)
 {
-	char1 = 'a';
+	char char1 = 'a';
 	while (char1 <= 'z')
 	{
-		_putchar (char1);
+		putchar (char1);
 		char1++;
 	}
 	return (char1);
 }
 int main(void)
 {
-	print_alphabet(void);
-	_putchar ('\n');
+	print_alphabet();
+	putchar ('\n');
 	return (0);
 }
