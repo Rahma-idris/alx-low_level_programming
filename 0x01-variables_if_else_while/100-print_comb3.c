76917ebd1b1f279b62271afb2ee7e67504fe9cd7
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-20 01:56:06 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,8 +2,7 @@
 /**
  * main - Print a combination of all digits
  *
- * Return
- * : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,15 +10,17 @@ int main(void)
 	int secondNum;
 
 		for (firstNum = 0; firstNum <= 9; firstNum++)
-		for (secondNum = firstNum + 1; secondNum <= 10; secondNum++)
 		{
-			putchar ((firstNum % 10) + '0');
-			putchar ((secondNum % 10) + '0');
-			if (firstNum == 8 && secondNum == 9)
-				continue;
-			putchar (',');
-			putchar (',');
-		}
+			for (secondNum = firstNum + 1; secondNum <= 10; secondNum++)
+			{
+				putchar ((firstNum % 10) + '0');
+				putchar ((secondNum % 10) + '0');
+				if (firstNum == 8 && secondNum == 9)
+					continue;
+				putchar (',');
+				putchar (',');
+			}
 		putchar('\n');
+		}
 		return (0);
 }
