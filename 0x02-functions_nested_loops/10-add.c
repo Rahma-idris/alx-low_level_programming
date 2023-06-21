966a37687cac0c22d9e2520fcacab3daa257f228
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-21 09:26:56 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * add - Function that adds two integers and returns the result.
- *@i - First value to be added
- *@j - Second value to be added
+ *@i: First value to be added
+ *@j: Second value to be added
  */
 int add(int i, int j)
 {
 	int sum = i + j;
 
-	return (sum);
+	return sum;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,5 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int i, int j;)
+int add(int i, int j);
 #endif /*MAIN_H*/
