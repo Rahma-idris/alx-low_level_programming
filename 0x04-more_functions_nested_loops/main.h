5af3ca57ddd5bb42191793aae7763aa88edd7703
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-22 09:00:37 -0700

    0-putchar.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -10,4 +11,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
