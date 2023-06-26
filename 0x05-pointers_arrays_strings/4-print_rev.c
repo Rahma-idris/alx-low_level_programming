d611a6e83931816cff1769527ee93f4152dbe0b8
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-26 14:27:38 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev - to print string in reverse
+ *
+ *@s: string.
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int l;
+
+	while (*s != 0)
+	{
+		l++;
+	}
+	for (l = 0; l = i - 1; l--)
+	{
+		_putchar (str[l]);
+	}
+	_putchar ('\n');
+}
