1c7494e05535f0786e2efec2fbc60dde484dc759
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-26 13:17:45 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 - Points the value 98 to n.
+ *
+ *@n: the pointer.
+ *Return: no return.
+ */
+void reset_to_98(int *n)
+{
+	int *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ *swap_int - To swap the numbers of a and b.
+ *
+ *@a: integer a.
+ *@b: integer b.
+ *Return: no return.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
