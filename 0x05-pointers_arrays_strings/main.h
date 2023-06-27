da2bbe0235dd296d2249351dc1121d85e5c5466b
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-26 17:03:22 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void_puts(char *str);
+void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
