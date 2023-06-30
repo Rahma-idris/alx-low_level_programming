7ed6eefad51309e65b3ebec4fa1b5d6cf0535546
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-06-30 14:14:28 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,34 +5,46 @@
  * @s: input string.
  * Return: the pointer to dest.
  */
-
-char *string_toupper(char *s)
+char *cap_string(char *str)
 {
+
 	int index = 0;
 
 	while (str[index])
 	{
+
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
 
+				str[index - 1] == '\t' ||
+
+				str[index - 1] == '\n' ||
+
+				str[index - 1] == ',' ||
+
+				str[index - 1] == ';' ||
+
+				str[index - 1] == '.' ||
+
+				str[index - 1] == '!' ||
+
+				str[index - 1] == '?' ||
+
+				str[index - 1] == '"' ||
+
+				str[index - 1] == '(' ||
+
+				str[index - 1] == ')' ||
+
+				str[index - 1] == '{' ||
+
+				str[index - 1] == '}' ||
+
+				index == 0)
+			str[index] -= 32;
 		index++;
 	}
-
 	return (str);
 }
