29134b7cc4806e20b12feceb2b6af582bdc5499e
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-04 13:48:45 -0700

    100-set_string

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,8 +15,10 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i * (size + 1)];  // Increment the diagonal element for sum1
-		sum2 += a[(i + 1) * (size - 1)];  // Increment the diagonal element for sum2
+		sum1 += a[i * (size + 1)];
+		/* Increment the diagonal element for sum1*/
+		sum2 += a[(i + 1) * (size - 1)];
+		/* Increment the diagonal element for sum2*/
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
