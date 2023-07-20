3401a039aad1a7d2ec6eaf30edf3fedc55a4dc92
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-19 17:47:47 -0700

    2-int_index

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- *array_iterator - Executes a function given as a parameter on 
+ *array_iterator - Executes a function given as a parameter on
  *each element of an array
  *@array: The array.
  * @size: The size of array.
@@ -15,6 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
+
 	for (i = 0; i < size; i++)
-	       action(array[i])	;
+		{
+			action(array[i]);
+		}
 }
