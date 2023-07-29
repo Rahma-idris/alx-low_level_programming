c2be01eab6a7d381b357a08070b20ae66ab195f7
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-07-28 20:27:34 -0700

    100-first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void __attribute___((constructor)) prequel(void);
  */
 void prequel(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf("\nI bore my house upon my back!\n");
 }
