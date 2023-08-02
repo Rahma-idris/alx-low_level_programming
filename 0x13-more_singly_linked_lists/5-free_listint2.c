0a36eb208d63a7d4a2d4228f395132d677210b42
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-08-02 08:05:15 -0700

    Another uodate

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head ==NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -19,5 +19,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-	head = n=NULL;
+	head = NULL;
 }		
