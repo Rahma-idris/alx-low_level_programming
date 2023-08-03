1e2939940645086e5be4378ce83bba2c229d1d81
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-08-02 21:16:37 -0700

    Another fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,6 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp;
 	listint_t *copy = *head;
+	unsigned int node;
 
 	if (copy == NULL)
 		return (-1);
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -15,7 +15,7 @@ int sum_listint(listint_t *head)
 	while (head != NULL)
 	{
 		sum += head->n;
-		head = head head->next;
+		head = head->next;
 	}
 	return (sum);
 }
