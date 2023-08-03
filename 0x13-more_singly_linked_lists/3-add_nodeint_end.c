5967c71e22a838e3087a162ba64c742866fcb0e6
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-08-02 21:24:33 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	 if (*head = NULL)
+	 if (*head == NULL)
 		 *head = new;
 	 ele
 	 {
@@ -28,5 +28,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			 last = last->next;
 		 last->next = new;
 	 }
-	 return (*head)
+	 return (*head);
 }
